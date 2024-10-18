47333586b6b7aaf8671ed8824fda2cb6d2c841a1
Gennadiy Saranchuk <alooha.broo@gmail.com>
2024-10-18 12:45:29 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Weather from "./components/Weather.vue";
+</script>
+
+<template>
+  <section class="section-header"></section>
+  <section class="section-main">
+    <Weather />
+  </section>
+  <section class="section-footer"></section>
+</template>
+
+<style scoped></style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="wrapper">
+    <h1>Погодное приложение</h1>
+    <p>Узнать погоду в вашем городе</p>
+  </div>
+</template>
